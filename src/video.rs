198b7e9bf394805b3f8fd19d7be473ee784029cd
umoho <my.umoho@hotmail.com>
2026-01-03 19:55:43 +0800

    feat: use `cairooverlay` to draw lines

--- src/video.rs
+++ src/video.rs
@@ -10,7 +10,8 @@ pub fn spawn_gst_thread(buffer: Arc<Mutex<Option<egui::ColorImage>>>) {
     std::thread::spawn(move || {
         // 采集 RGBA 原始像素，适配 egui
         // let pipeline_str = "videotestsrc ! video/x-raw,format=RGBA,width=1280,height=720 ! appsink name=sink sync=false";
-        let pipeline_str = "srtsrc uri=\"srt://:7000?mode=listener\" ! tsdemux ! queue ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! video/x-raw,format=RGBA,width=1280,height=720 ! appsink name=sink sync=false";
+        let pipeline_str = "videotestsrc ! video/x-raw,width=1280,height=720 ! videoconvert ! cairooverlay name=overlay ! videoconvert ! appsink name=sink sync=false";
+        // let pipeline_str = "srtsrc uri=\"srt://:7000?mode=listener\" ! tsdemux ! queue ! h264parse ! avdec_h264 ! videoconvert ! videoscale ! video/x-raw,format=RGBA,width=1280,height=720 ! appsink name=sink sync=false";
         let pipeline = gst::parse::launch(pipeline_str).expect("Pipeline error");
         let sink = pipeline
             .downcast_ref::<gst::Bin>()
@@ -45,8 +46,53 @@ pub fn spawn_gst_thread(buffer: Arc<Mutex<Option<egui::ColorImage>>>) {
                 .build(),
         );
 
+        let overlay = pipeline
+            .dynamic_cast_ref::<gst::Pipeline>()
+            .unwrap()
+            .by_name("overlay")
+            .unwrap();
+        overlay.connect("draw", false, draw_overlay);
+
         pipeline.set_state(gst::State::Playing).ok();
         let bus = pipeline.bus().unwrap();
         for _msg in bus.iter_timed(gst::ClockTime::NONE) {}
     });
+}
+
+fn draw_overlay(values: &[cairo::glib::Value]) -> Option<cairo::glib::Value> {
+    // values[0]: cairooverlay 元素本身
+    // values[1]: cairo::Context
+    // values[2]: timestamp
+    // values[3]: duration
+    let _overlay = values[0].get::<gst::Element>().unwrap();
+    let cr = values[1].get::<cairo::Context>().unwrap();
+
+    // 获取当前分辨率
+    // NOTE: 可以缓存这些信息以提高性能
+    let pad = _overlay.static_pad("sink").unwrap();
+    let caps = pad.current_caps().unwrap();
+    let structure = caps.structure(0).unwrap();
+    let width = structure.get::<i32>("width").unwrap() as f64;
+    let height = structure.get::<i32>("height").unwrap() as f64;
+
+    // --- 开始绘制 ---
+
+    // 绘制三分参考线
+    cr.set_source_rgba(1.0, 1.0, 1.0, 0.5); // 白色，0.5 透明度
+    cr.set_line_width(2.0);
+    // 垂直线
+    for i in 1..3 {
+        let x = width / 3.0 * i as f64;
+        cr.move_to(x, 0.0);
+        cr.line_to(x, height);
+    }
+    // 水平线
+    for i in 1..3 {
+        let y = height / 3.0 * i as f64;
+        cr.move_to(0.0, y);
+        cr.line_to(width, y);
+    }
+    cr.stroke().expect("Stroke failed");
+
+    None
 }
